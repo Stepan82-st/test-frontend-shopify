fa57683f3f186243169a76d145f0174990d81e35
Stepan Bryl <bryl.step@tut.by>
2022-08-10 13:11:36 +0300

    feat: task2

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,34 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+const btnOpenColl = document.querySelector('.collapsible__button');
+const spans = document.querySelectorAll('.collapsible__button > span');
+const text = document.querySelector('.collapsible__content');
+
+let isPlay = false;
+btnOpenColl.addEventListener('click', function(){  
+    if(!isPlay){
+    spans[1].style.display = "none";
+    spans[0].style.display= "block";
+    text.style.display = "block";
+    const aliceTumbling = [
+        { color: 'red'}, 
+        { color: '#431236', offset: 0.3},
+        { color: 'green' }
+      ];
+      
+      const aliceTiming = {
+        duration: 3000,
+        iterations: Infinity
+      }
+      text.animate(
+        aliceTumbling, 
+        aliceTiming
+      )
+    
+    isPlay = true;
+    }else{
+        spans[0].style.display = "none";
+        spans[1].style.display = "block";
+        text.style.display = "none";
+        isPlay = false;
+    }
+})
+
